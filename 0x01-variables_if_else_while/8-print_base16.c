7d3cc5b57414d9f1d9e6a9f9f685539a12f52e12
dija205 <khadijanana205@gmail.com>
2023-09-09 16:16:30 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 48; j < 58; j++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j < 103; j++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
