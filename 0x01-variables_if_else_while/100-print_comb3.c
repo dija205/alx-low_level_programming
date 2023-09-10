4fd8773ac4b81c26d2b7ec808eb6abeb0224a123
dija205 <khadijanana205@gmail.com>
2023-09-10 13:14:59 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,7 +8,7 @@ int main(void)
 int i,j;
 for (i = 0; i < 10; i++) {
  for (j = i + 1; j < 10; j++) {
-  printf("%d%d ", i, j);
+  printf("%d%d", i, j);
   if (i != 8 || j != 9) {
     printf(", ");
            }
