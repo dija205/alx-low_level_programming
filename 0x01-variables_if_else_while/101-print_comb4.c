01b70773d560d6c6090ba05b74d59eda8bdde761
dija205 <khadijanana205@gmail.com>
2023-09-11 13:11:39 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,9 +11,9 @@ int i, j;
 int k;
 for (i = 0; i < 10; i++)
 {
-for (j = i+1; j < 10; j++)
+for (j = i + 1; j < 10; j++)
 {
-for (k = j+1; k < 10 ; k++)
+for (k = j + 1; k < 10 ; k++)
 {
 printf("%d%d%d", i, j, k);
 if (i != 7 || j != 8 || k != 9)
