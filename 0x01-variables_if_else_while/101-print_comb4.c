166d3ab77cfc0463ccc07f193162e1d395227f9f
dija205 <khadijanana205@gmail.com>
2023-09-11 12:54:43 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**                                                             
+ * main - entry point                                               
+ * Return : Always 0 (success)                                
+ */
+int main(void)
+{
+int i, j;
+int k;
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+    for(k=j+1 ;k<10 ;k++){
+printf("%d%d%d", i, j, k);
+if (i != 7 || j != 8 || k!=9)
+{
+printf(", ");
+}
+}
+}
+}
+printf("\n");
+return (0);
+}
