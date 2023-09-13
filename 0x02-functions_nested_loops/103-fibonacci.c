fc758ad76169f7500fca73e11bee9dc60ceea0e8
dija205 <khadijanana205@gmail.com>
2023-09-13 13:26:22 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - prints sum of even fibonacci
+*suit elements
+*fibonacci suit numbers
+*Return: return 0
+*/
+int main(void)
+{
+unsigned long a = 0, b = 1, c = 0, sum = 0;
+while (c <= 4000000)
+{
+c = a +b;
+a = b;
+b = c;
+
+if ((a % 2) == 0)
+sum += n1;
+
+}
+printf("%ld\n", sum);
+return (0);
+}
