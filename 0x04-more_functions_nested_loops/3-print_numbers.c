086b671dd65dff1f398c04f6bfdf0b6af02f36f5
dija205 <khadijanana205@gmail.com>
2023-09-15 13:31:26 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return:no returns 
+*/
+void print_numbers(void)
+{
+int i = 48;
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar(10);
+}
