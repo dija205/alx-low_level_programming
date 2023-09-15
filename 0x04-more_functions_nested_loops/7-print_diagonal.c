a86a938bba774704f8d3b4ad3cd4eecb2d6bfd8a
dija205 <khadijanana205@gmail.com>
2023-09-15 14:02:39 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,6 +12,7 @@ if (n > 0)
 for (l = 0; l < n; l++)
 {
 for (s = 0; s < l; s++)
+{
 _putchar(' ');
 
 _putchar('\\');
@@ -19,6 +20,7 @@ _putchar('\\');
 if (l == (n - 1))
 continue;
 _putchar('\n');
+}
 }
 }
 _putchar('\n');
