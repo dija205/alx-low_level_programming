d4d79d94bf444b162d1549ef6c52c5cc14a40b3c
dija205 <khadijanana205@gmail.com>
2023-09-22 14:42:22 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,7 +10,7 @@ char *leet(char *str)
 	int i1 = 0, i2;
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (str[++index1])
+	while (str[++i1])
 	{
 		for (i2 = 0; i2 <= 7; i2++)
 		{
